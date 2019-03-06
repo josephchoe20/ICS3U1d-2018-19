b843a448b559a05092ed9f06528593f638ef80a2
robuntu <robuntu@stro.ycdsb.ca>
2019-03-06 11:10:24 -0500

    Commit

--- Working/Drawing_with_function/intro_to_functions.py
+++ Working/Drawing_with_function/intro_to_functions.py
@@ -1,16 +1,15 @@
 import arcade
-
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
 
 # define your draw functions
-def draw_cloud() :
-    arcade.draw_circle_filled(590, 400, 40, arcade.color.WHITE_SMOKE)
-    arcade.draw_circle_filled(540, 450, 53, arcade.color.WHITE_SMOKE)
-    arcade.draw_circle_filled(540, 385, 35, arcade.color.WHITE_SMOKE)
-    arcade.draw_circle_filled(485,400, 40, arcade.color.WHITE_SMOKE)
+def draw_cloud(x, y) :
+    arcade.draw_circle_filled(x + 50, y, 40, arcade.color.WHITE_SMOKE)
+    arcade.draw_circle_filled(x, y + 50, 53, arcade.color.WHITE_SMOKE)
+    arcade.draw_circle_filled(x, y - 15, 35, arcade.color.WHITE_SMOKE)
+    arcade.draw_circle_filled(x - 55, y, 40, arcade.color.WHITE_SMOKE)
 
-def draw_hill ():
+def draw_hill():
     arcade.draw_circle_filled(25, 100, 200, arcade.color.DARK_SPRING_GREEN)
     arcade.draw_circle_filled(125, 100, 150, arcade.color.DARK_SPRING_GREEN)
     arcade.draw_circle_filled(300, 50, 100, arcade.color.DARK_SPRING_GREEN)
@@ -20,15 +19,15 @@ def draw_hill ():
     arcade.draw_circle_filled(550, 50, 100, arcade.color.DARK_SPRING_GREEN)
     arcade.draw_circle_filled(450, 00, 100, arcade.color.DARK_SPRING_GREEN)
 
-def draw_tree ():
+def draw_tree(x, y) :
     # draw trunk
-    arcade.draw_rectangle_filled(427, 100, 20, 80, arcade.color.DARK_BROWN)
+    arcade.draw_rectangle_filled(x + 27, y - 50, 20, 80, arcade.color.DARK_BROWN)
     # draw hole in trunk
-    arcade.draw_circle_filled(427.5, 95, 6, arcade.color.BLACK_BEAN)
+    arcade.draw_circle_filled(x + 27.5, y- 55, 6, arcade.color.BLACK_BEAN)
     # draw leaves
-    arcade.draw_circle_filled(447, 140, 30, arcade.color.APPLE_GREEN)
-    arcade.draw_circle_filled(407, 140, 30, arcade.color.APPLE_GREEN)
-    arcade.draw_circle_filled(427, 170, 40, arcade.color.APPLE_GREEN)
+    arcade.draw_circle_filled(x + 47, y - 10, 30, arcade.color.APPLE_GREEN)
+    arcade.draw_circle_filled(x + 7, y - 10, 30, arcade.color.APPLE_GREEN)
+    arcade.draw_circle_filled(x + 27, y + 20, 40, arcade.color.APPLE_GREEN)
     # draw apples :
     arcade.draw_circle_filled(410, 150, 5, arcade.color.BRICK_RED)
     arcade.draw_circle_filled(440, 150, 5, arcade.color.BRICK_RED)
@@ -46,14 +45,16 @@ def main():
 
    # call your draw functions
 
-    draw_cloud()
-    draw_hill()
+    draw_cloud(500, 300)
+    draw_cloud(300, 350)
+    draw_cloud(650, 450)
+    draw_cloud(200, 500)
+    draw_hill(400, 150)
     draw_tree()
 
     # Finish and run
     arcade.finish_render()
     arcade.run()
-
 
 # Call the main function to get the program started.
 main()
